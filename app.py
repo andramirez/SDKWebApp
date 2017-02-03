2d51bf4e1ddeb13394ad76cf7859030b74536dc7
andramirez <andramirez@csumb.edu>
2017-02-03 21:30:00 +0000

    update

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-import flask, random, os, tweepy, json, requests
+import flask, random, os, tweepy, json, requests, re
 
 app = flask.Flask(__name__)
 
@@ -8,15 +8,17 @@ app = flask.Flask(__name__)
 
 def index():
 #****************************************************GITHUB****************************************
-    phrases = ['nature', 'beauty', 'animals']
+    phrases = ['nature', 'sky']
     phrase = random.choice(phrases)
     #search url to bring up certain images based on query
     search_url="https://api.gettyimages.com:443/v3/search/images?exclude_nudity=true&file_types=jpg&minimum_size=large&orientations=Horizontal&sort_order=best_match&number_of_people=none&phrase=" + phrase
     
-    # search_url = "https://api.gettyimages.com:443/v3/search/images?phrase=star%20trek%20the%20original%20series"
+    # search_url = "https://api.gettyimages.com:443/v3/search/images?phrase=astronomy&number_of_people=none"
     #api key
-    # key = os.getenv("getty_key")
+
     headers1 = {'Api-Key': '42hg9wahfqwvspm4dwa2vs7g'}
+    # headers1 = {'Api-Key': os.getenv("getty_key")}
+
     #result of image search
     result = requests.get(search_url, headers=headers1)
     #result in json to get page ID
@@ -37,6 +39,11 @@ def index():
     acc_token = "824355548501987328-slJGgxFlWbfetxRZdN3iutYTA21tg7v"
     acc_token_sec = "8L8HuqrVJY9ZM9ezb8R2NaCw2kmEBTJRHO82Js98JCnrp"
     
+    # con_key = os.getenv("twitterkey")
+    # sec_con_key = os.getenv("twitterkey_sec")
+    # acc_token = os.getenv("twitter_token")
+    # acc_token_sec = os.getenv("twitter_token_sec")
+    
     users = ['experiencedquot', 'QuotesDetail', '_Famouss_Quotes', 'quotedefamous', 'motivational', 'philosophy_muse']
     user = random.choice(users)
     
@@ -50,10 +57,9 @@ def index():
                                 count = 200,
                                 lang = "en")
     tweet1 = random.choice(tweets)
-    while "https" in tweet1.text:
-        tweet1 = random.choice(tweets)
+    result = re.sub(r"http\S+", "", tweet1.text)
         
-    return flask.render_template("index.html", text = tweet1.text, author = user, image = image1) ## sends tweet and image to html page via flask
+    return flask.render_template("index.html", text = result, author = user, image = image1) ## sends tweet and image to html page via flask
     
     
 app.run(

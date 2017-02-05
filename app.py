14e552597c5124e291524694c865f571109ce17a
andramirez <andramirez@csumb.edu>
2017-02-05 20:36:51 +0000

    Update

--- app.py
+++ app.py
@@ -51,7 +51,7 @@ def index():
                                 count = 200,
                                 lang = "en")
     tweet1 = random.choice(tweets) # choose random tweets
-    if "http\S+" in tweet1.text:
+    if "https:\S+" in tweet1.text:
         tweet1 = random.choice(tweets) # choose random tweets
     
     url = user + "/status/"+ tweet1.id_str
